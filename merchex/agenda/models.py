3186a815f3e6e9a42a9f7ea7669b287011f6312f
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2024-02-12 13:44:03 +0100

    popup inviter un patient & finition

--- merchex/agenda/models.py
+++ merchex/agenda/models.py
@@ -6,7 +6,7 @@ class Medecin(AbstractUser):
     tel = models.CharField(max_length=15, blank=True, null=True)
     profession = models.CharField(max_length=255, blank=True, null=True)
     color = models.CharField(max_length=20, blank=True, null=True)
-    fixed_schedule = models.TextField(blank=True, null=True)
+    #fixed_schedule = models.TextField(blank=True, null=True, default="")
     address_of_office = models.TextField(blank=True, null=True)
 
 
