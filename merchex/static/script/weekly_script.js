97d9997121e228a961666fc0350e22624080cd7c
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2024-02-26 18:29:34 +0100

    popup available click RDV, lundi available

--- merchex/static/script/weekly_script.js
+++ merchex/static/script/weekly_script.js
@@ -1,52 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Sélectionnez le premier bouton image
-    const firstButton = document.getElementById('first-button');
-    // Définir une variable pour suivre l'état actuel de l'image du premier bouton
-    let isFirstImage = true;
+
+    var inputs = document.getElementsByClassName('popup-button');
     
-    // Écoutez le clic sur le premier bouton image
-    firstButton.addEventListener('click', function() {
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('click', function() {
 
-        event.stopPropagation();
-        var hourSlot = this.parentElement;
-        if (isFirstImage) {
-            firstButton.style.backgroundImage = "url('../../static/img/weekly/unlock.png')";
-            
-            hourSlot.style.backgroundColor = 'white';
-        } else {
-            firstButton.style.backgroundImage = "url('../../static/img/weekly/lock.png')";
-            hourSlot.style.backgroundColor = '#f0f0f0';
-        }
-        // Inversez l'état actuel de l'image du premier bouton
-        isFirstImage = !isFirstImage;
-    });
-
-
-    const secondButton = document.getElementById('second-button');
-    let isFirstImageSecond = true
-    // Écoutez le clic sur le premier bouton image
-    secondButton.addEventListener('click', function() {
-
-        event.stopPropagation();
-
-        var hourSlot = this.parentElement;
-        if (isFirstImageSecond) {
-            // Si c'est la première image, changez-la en deuxième image
-            secondButton.style.backgroundImage = "url('../../static/img/weekly/bin.png')";
-            hourSlot.style.backgroundColor = 'lightgreen';
-        } else {
-            secondButton.style.backgroundImage = "url('../../static/img/weekly/add.png')";
-            hourSlot.style.backgroundColor = '#f0f0f0';
-        }
-        // Inversez l'état actuel de l'image du premier bouton
-        isFirstImageSecond = !isFirstImageSecond;
-    });
- 
-    
-    document.getElementById('popup-button').addEventListener('click', function() {
-        
-        document.getElementById('myPopup').style.display = 'block';
-    });
+            document.getElementById('myPopup').style.display = 'block';
+        });
+    }
 
     document.getElementById('close').addEventListener('click', function() {
 
